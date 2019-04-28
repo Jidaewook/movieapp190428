3fda0ee50e9f1b15819ca297551039fa2af5f70f
Jay <psatdoctor@naver.com>
2019-04-28 16:04:42 +0900

    Pre Loading Assets

--- App.js
+++ App.js
@@ -1,13 +1,40 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {AppLoading, Font} from 'expo';
+import {Ionicons} from "@expo/vector-icons";
 
 export default class App extends React.Component {
+  state={
+    loaded: false
+  };
+
+  handleError = error => console.log(error);
+  handleLoaded=()=> this.setState({loaded:true});
+
+loadAssets = async()=>{
+  await Font.loadAsync({
+    ...Ionicons.font
+  });
+};
+
   render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-    );
+    const{loaded}=this.state;
+    if(loaded){
+      return (
+        <View style={styles.container}>
+          <Text>Open up App.js to start working on your app!</Text>
+        </View>
+      );
+    } else{
+      return(
+        <AppLoading
+          startAsync={this.loadAssets}
+          onFinish={this.handleLoaded}
+          onError={this.handleError}
+
+        />
+      );
+    }
   }
 }
 
